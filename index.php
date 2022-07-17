12f797923e5a43582b0b5656a86dfe5826942955
codewithchinekwe <codewithchinekwe@gmail.com>
2022-07-17 06:40:38 +0100

    added code for If Statement in PHP

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
 </head>
 
 <body>
+     <h2>Follow each link to the page example</h2>
+     <ul>
+          <li><a href="array.php">Simple Array and Printouts</a></li>
+          <li><a href="forloop.php">Simple For Loop</a></li>
+          <li><a href="dowhileloop.php">Simple Do While Loop</a></li>
+          <li><a href="whileloop.php">Simple While Loop</a></li>
+          <li><a href="ifstatement.php">Simple If Statement</a></li>
+          <li><a href="switchstatement.php">Simple Switch Statement</a></li>
+     </ul>
      <!-- Basic HTML -->
      <h1>Hello HTML - PHP Primer</h1>
 
